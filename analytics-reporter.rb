7b00791a65743de45175ee07b79d44f8dd26dd7f
Rasan Rasch <rasan@nyu.edu>
2017-03-07 16:44:59 -0800

    Add fiscal dates

--- analytics-reporter.rb
+++ analytics-reporter.rb
@@ -8,14 +8,16 @@ require 'google/apis/analytics_v3'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require 'fileutils'
+require 'fiscali'
 require 'mail'
 require 'optparse'
 require 'thor'
 require 'yaml'
 
 config = {
-  :start   => Chronic.parse('last week').strftime('%Y-%m-%d'),
-  :end     => Time.now.strftime('%Y-%m-%d'),
+#   :start   => Chronic.parse('last week').strftime('%Y-%m-%d'),
+#   :end     => Time.now.strftime('%Y-%m-%d'),
+  :fiscal_qtr => Time.now.strftime('Q1/%y'),
   :outfile => 'out.csv',
 }
 
@@ -27,12 +29,16 @@ OptionParser.new do |opts|
 
   opts.banner = "Usage: #{$0} [options]"
 
-  opts.on('-s', '--start START_DATE', 'Start date for query') do |s|
-    config[:start] = s
-  end
+#   opts.on('-s', '--start START_DATE', 'Start date for query') do |s|
+#     config[:start] = s
+#   end
+# 
+#   opts.on('-e', '--end END_DATE', 'End date for query') do |e|
+#     config[:end] = e
+#   end
 
-  opts.on('-e', '--end END_DATE', 'End date for query') do |e|
-    config[:end] = e
+  opts.on('-f', '--fiscal-qtr', 'Fiscal Quarter, e.g. Q4/16') do |f|
+    config[:fiscal_qtr] = f
   end
 
   opts.on('-o', '--outfile OUTFILE', 'Output CSV file') do |o|
@@ -45,6 +51,22 @@ OptionParser.new do |opts|
   end
 
 end.parse!
+
+now = Date.today
+
+Date.fiscal_zone = :us
+Date.fy_start_month = 9
+
+config[:start] =  now.previous_financial_quarter
+config[:end] = config[:start].end_of_financial_quarter
+
+config[:prev_start] = config[:start].previous_financial_quarter
+config[:prev_end] = config[:prev_start].end_of_financial_quarter
+
+puts config[:prev_start]
+puts config[:prev_end]
+puts config[:start]
+puts config[:end]
 
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
 APPLICATION_NAME = 'Analytics Reporter'
@@ -89,35 +111,79 @@ service.authorization = authorize
 
 csv = CSV.open(config[:outfile], 'w')
 
-service.list_profiles('~all', '~all').items.each do |profile|
-  view_name = profile.name.dup
-  if view_name.sub!(' (master view)', '').nil?
-    #puts "Ignoring #{view_name}"
-    next
-  end
-  puts "Querying master view: #{view_name}"
-  puts profile.inspect
-  dimensions = %w(ga:date)
-  metrics = %w(ga:sessions ga:users ga:newUsers ga:pageviews)
-  sort = %w(ga:date)
-  result = service.get_ga_data("ga:#{profile.id}",
-                                 config[:start],
-                                 config[:end],
-                                 metrics.join(','))
-                                 #dimensions: dimensions.join(','),
-                                 #sort: sort.join(','))
-
-  data = []
-  data.push(result.column_headers.map { |h| h.name })
-  data.push(*result.rows)
-  puts "FOO", result.inspect
-  #if result.total_results > 0
-  csv << result.rows[0]
-  Thor.new.print_table(data)
-  #end
-  break
+def fmt_date(date)
+  date.strftime('%Y-%m-%d')
 end
 
+# class Numeric
+#   def percent_of(n)
+#     self.to_f / n.to_f * 100.0
+#   end
+# end
+
+def calc_percent(r1, r2, col_num)
+  val1 = r1.rows[0][col_num].to_f
+  val2 = r2.rows[0][col_num].to_f
+  sprintf('%.2f', ((val2 - val1) / val1) * 100)
+end
+
+csv << ['DLTS collections quarterly report']
+csv << ['Year:', "FY#{now.financial_year}"]
+csv << ['Quarter:', now.financial_quarter.split.first]
+csv << ['Account',
+        'Property',
+        '# of sessions',  'Chg from prev qtr',
+        '# of users',     'Chg from prev qtr',
+        '# of pageviews', 'Chg from prev qtr'
+       ]
+
+metrics = %w(ga:sessions ga:users ga:pageviews)
+metrics_str = metrics.join(',')
+
+service.list_accounts.items.each do |account|
+
+  puts account.name
+
+  service.list_profiles(account.id, '~all').items.each do |profile|
+    view_name = profile.name.dup
+    if view_name.sub!(' (master view)', '').nil?
+      #puts "Ignoring #{view_name}"
+      next
+    end
+    puts "Querying master view: #{view_name}"
+    puts profile.inspect
+    prev_result = service.get_ga_data("ga:#{profile.id}",
+                                   fmt_date(config[:prev_start]),
+                                   fmt_date(config[:prev_end]),
+                                   metrics_str)
+  
+    result = service.get_ga_data("ga:#{profile.id}",
+                                   fmt_date(config[:start]),
+                                   fmt_date(config[:end]),
+                                   metrics_str)
+    puts prev_result.rows.inspect
+    puts result.rows.inspect
+    data = []
+    data.push(account.name)
+    data.push(view_name)
+    (0..2).each do |n|
+      data.push(result.rows[0][n])
+      data.push(calc_percent(prev_result, result, n))
+    end
+  
+  #   data.push(result.column_headers.map { |h| h.name })
+  #   data.push(*result.rows)
+  #   puts "FOO", result.inspect
+  #   #if result.total_results > 0
+    csv << data
+    #Thor.new.print_table(data)
+  #   #end
+  end
+  
+  break
+
+end
+  
 desc = "Analytics Report for #{config[:start]} to #{config[:end]}"
 
 mail = Mail.new do

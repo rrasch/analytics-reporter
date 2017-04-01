a9f307921746687185f6921e5b601b19956045e4
Rasan Rasch <rasan@nyu.edu>
2017-04-01 12:14:07 -0400

    Add some spreadsheet formatting.

--- writer.rb
+++ writer.rb
@@ -14,11 +14,30 @@ class ReportWriter
     @csv = CSV.open(@csv_file, 'w')
     @xls_file = "#{basename}.xls"
     @xls = Spreadsheet::Workbook.new.create_worksheet
+    @fmt_perc_dec = Spreadsheet::Format.new :color => :red,
+                                            :weight => :bold
+    @fmt_perc_inc = Spreadsheet::Format.new :color => :green,
+                                            :weight => :bold
+    @fmt_num      = Spreadsheet::Format.new :weight => :bold
   end
 
   def add_row(row)
     @csv << row
-    @xls.row(@xls.row_count).concat(row)
+    row_num = @xls.row_count
+    @xls.row(row_num).concat(row)
+    row.each_with_index do |val, i|
+      val_str = val.to_s
+      fmt = nil
+      if val_str.starts_with?('-')
+        fmt = @fmt_perc_dec
+        puts fmt.inspect
+      elsif val_str.ends_with?('%')
+        fmt = @fmt_perc_inc
+      elsif val_str =~ /^\d$/
+        fmt = @fmt_num
+      end
+      @xls.row(row_num).set_format(i, fmt) if fmt
+    end
   end
 
   def files

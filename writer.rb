2fcab69b22a7840001efa92df31158eff5c42a54
Rasan Rasch <rasan@nyu.edu>
2017-04-03 07:21:51 -0400

    Improve formatting. Add autofit for col widths.

--- writer.rb
+++ writer.rb
@@ -14,9 +14,9 @@ class ReportWriter
     @csv = CSV.open(@csv_file, 'w')
     @xls_file = "#{basename}.xls"
     @xls = Spreadsheet::Workbook.new.create_worksheet
-    @fmt_perc_dec = Spreadsheet::Format.new :color => :red,
+    @fmt_perc_dec = Spreadsheet::Format.new :color  => :red,
                                             :weight => :bold
-    @fmt_perc_inc = Spreadsheet::Format.new :color => :green,
+    @fmt_perc_inc = Spreadsheet::Format.new :color  => :green,
                                             :weight => :bold
     @fmt_num      = Spreadsheet::Format.new :weight => :bold
   end
@@ -28,15 +28,47 @@ class ReportWriter
     row.each_with_index do |val, i|
       val_str = val.to_s
       fmt = nil
-      if val_str.starts_with?('-')
-        fmt = @fmt_perc_dec
-        puts fmt.inspect
-      elsif val_str.ends_with?('%')
-        fmt = @fmt_perc_inc
-      elsif val_str =~ /^\d$/
+      if val_str =~ /^(.*)%$/
+        percent = $1.to_f
+        if percent < 0
+          fmt = @fmt_perc_dec
+        elsif percent > 0
+          fmt = @fmt_perc_inc
+        end
+      elsif val_str =~ /^\d+$/
         fmt = @fmt_num
       end
       @xls.row(row_num).set_format(i, fmt) if fmt
+    end
+  end
+
+  # http://stackoverflow.com/questions/11621919/using-ruby-spreadsheet-gem-is-there-a-way-to-get-cell-to-adjust-to-size-of-cont
+  def autofit
+    (0...@xls.column_count).each do |col|
+      high = 1
+      row = 0
+      @xls.column(col).each do |cell|
+        w = cell==nil || cell=='' ? 1 : cell.to_s.strip.split('').count+3
+        ratio = @xls.row(row).format(col).font.size/10
+        w = (w*ratio).round
+        if w > high
+          high = w
+        end
+        row=row+1
+      end
+      @xls.column(col).width = high
+    end
+    (0...@xls.row_count).each do |row|
+      high = 1
+      col = 0
+      @xls.row(row).each do |cell|
+        w = @xls.row(row).format(col).font.size+4
+        if w > high
+          high = w
+        end
+        col=col+1
+      end
+      @xls.row(row).height = high
     end
   end
 
@@ -46,6 +78,7 @@ class ReportWriter
 
   def close
     @cvs_close
+    autofit
     @xls.workbook.write(@xls_file)
   end
 

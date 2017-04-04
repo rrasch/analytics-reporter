fa3325b2e36e78b32f81b4fa8bbc0ef0ab72c1d6
Rasan Rasch <rasan@nyu.edu>
2017-04-04 07:05:51 -0400

    Ignore title row in autofit and format title row

--- writer.rb
+++ writer.rb
@@ -14,19 +14,20 @@ class ReportWriter
     @csv = CSV.open(@csv_file, 'w')
     @xls_file = "#{basename}.xls"
     @xls = Spreadsheet::Workbook.new.create_worksheet
-    @fmt_perc_dec = Spreadsheet::Format.new :color  => :red,
-                                            :weight => :bold
-    @fmt_perc_inc = Spreadsheet::Format.new :color  => :green,
-                                            :weight => :bold
-    @fmt_num      = Spreadsheet::Format.new :weight => :bold
+    @fmt_title    = Spreadsheet::Format.new :align     => :center
     @fmt_header   = Spreadsheet::Format.new :underline => :single
+    @fmt_perc_dec = Spreadsheet::Format.new :color     => :red,
+                                            :weight    => :bold
+    @fmt_perc_inc = Spreadsheet::Format.new :color     => :green,
+                                            :weight    => :bold
+    @fmt_num      = Spreadsheet::Format.new :weight    => :bold
   end
 
   def add_row_header(row)
     add_row(row, @fmt_header)
   end
 
-  def add_row(row, row_format=nil)
+  def add_row(row, row_format = nil)
     @csv << row
     row_num = @xls.row_count
     @xls.row(row_num).concat(row)
@@ -49,33 +50,39 @@ class ReportWriter
   end
 
   # http://stackoverflow.com/questions/11621919/using-ruby-spreadsheet-gem-is-there-a-way-to-get-cell-to-adjust-to-size-of-cont
-  def autofit
-    (0...@xls.column_count).each do |col|
-      high = 1
-      row = 0
-      @xls.column(col).each do |cell|
-        w = cell == nil || cell == '' ? 1
-          : cell.to_s.strip.split('').count + 3
-        ratio = @xls.row(row).format(col).font.size / 10
-        w = (w * ratio).round
-        if w > high
-          high = w
+  # Solution by CCinkosky
+  # Auto adjust width of columns and height of rows based on
+  # widest cell in a column and tallest cell in a row.
+  # This method will loop through each column in the worksheet, then
+  # loop through each cell in the column, finding the cell with the most
+  # characters (+3 for a little extra space) and adjusting according to
+  # that cell. It then does the same thing for the rows. This value
+  # works great for font size 10, but to make sure it gets a little
+  # bigger for larger fonts, it adjusts to the font size by calculating
+  # the scale.
+  def autofit(start_row = 0)
+    (0...@xls.column_count).each do |col_idx|
+      column_width = 0
+      (start_row...@xls.row_count).each do |row_idx|
+        cell_val = @xls[row_idx, col_idx].to_s
+        scale = @xls.row(row_idx).format(col_idx).font.size / 10
+        cell_width = ((cell_val.length + 3) * scale).round
+        if cell_width > column_width
+          column_width = cell_width
         end
-        row = row + 1
       end
-      @xls.column(col).width = high
+      @xls.column(col_idx).width = column_width
     end
-    (0...@xls.row_count).each do |row|
-      high = 1
-      col = 0
-      @xls.row(row).each do |cell|
-        w = @xls.row(row).format(col).font.size + 4
-        if w > high
-          high = w
+    (0...@xls.row_count).each do |row_idx|
+      row_height = 0
+      (0...@xls.column_count).each do |col_idx|
+        cell_val = @xls[row_idx, col_idx].to_s
+        cell_height = @xls.row(row_idx).format(col_idx).font.size + 4
+        if cell_height > row_height
+          row_height = cell_height
         end
-        col = col + 1
       end
-      @xls.row(row).height = high
+      @xls.row(row_idx).height = row_height
     end
   end
 
@@ -85,7 +92,9 @@ class ReportWriter
 
   def close
     @cvs_close
-    autofit
+    autofit(1)
+    @xls.merge_cells(0, 0, 0, @xls.column_count - 1)
+    @xls.row(0).set_format(0, @fmt_title)
     @xls.workbook.write(@xls_file)
   end
 

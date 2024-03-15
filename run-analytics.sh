b866b85424ea23802eca67bf1a1227d0ab968205
Rasan Rasch <rasan@nyu.edu>
2024-03-14 20:16:32 -0400

    Cron wrapper script for analytics scripts

---
+++ run-analytics.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_HOME=$HOME/work/analytics-reporter
+
+cd $SCRIPT_HOME
+git pull
+
+source "$HOME/.rvm/scripts/rvm"
+
+./storage-reporter.rb
+
+sleep 5
+
+./analytics-reporter.rb
+
+# unset rvm environment
+{ type -t __rvm_unload >/dev/null; } && __rvm_unload
+
+# unset other ruby vars
+unset GEM_HOME
+unset GEM_PATH
+unset RUBY_VERSION
+
+sleep 5
+
+source "$HOME/venv/analytics/bin/activate"
+
+./analytics-by-location-v4.py
+
+deactivate

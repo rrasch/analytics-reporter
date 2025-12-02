10bd16aaea85de63c2584218e1f645cc507ae270
Rasan Rasch <rasan@nyu.edu>
2025-12-02 15:46:59 -0500

    Remove 'Install' section from service file
    - Remove Install section from service file so scripts don't run at
      boot if they are not due to run
    - Set Persistent to true in timer file so that scripts can run if
      they were due to run while machine was down
    - Set log files

--- install-timer.sh
+++ install-timer.sh
@@ -6,19 +6,19 @@ SYSTEMD_DIR=$HOME/.config/systemd/user
 
 SYSTEMCTL="systemctl --user --no-pager"
 
-# APP_HOME=$HOME/work/analytics-reporter
 APP_HOME=$(dirname -- "$(readlink -f -- "$0")")
 
 export SYSTEMD_PAGER=""
-
-install -m 0644 $APP_HOME/analytics.{timer,service} $SYSTEMD_DIR
-
-perl -pi -e "s,<APP_HOME>,$APP_HOME," $SYSTEMD_DIR/analytics.service
 
 set +e
 $SYSTEMCTL stop analytics.timer
 $SYSTEMCTL disable analytics.service analytics.timer
 set -e
+
+install -m 0644 $APP_HOME/analytics.{timer,service} $SYSTEMD_DIR
+
+perl -pi -e "s,<APP_HOME>,$APP_HOME," $SYSTEMD_DIR/analytics.service
+perl -pi -e "s,<LOGDIR>,$HOME," $SYSTEMD_DIR/analytics.service
 
 $SYSTEMCTL daemon-reload
 

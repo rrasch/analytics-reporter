671efa6e9faa9ff7b15b80d34a48c19588e3b789
Rasan Rasch <rasan@nyu.edu>
2025-12-02 16:02:12 -0500

    Verify systemd files

--- install-timer.sh
+++ install-timer.sh
@@ -20,6 +20,8 @@ install -m 0644 $APP_HOME/analytics.{timer,service} $SYSTEMD_DIR
 perl -pi -e "s,<APP_HOME>,$APP_HOME," $SYSTEMD_DIR/analytics.service
 perl -pi -e "s,<LOGDIR>,$HOME," $SYSTEMD_DIR/analytics.service
 
+systemd-analyze verify $SYSTEMD_DIR/analytics.*
+
 $SYSTEMCTL daemon-reload
 
 $SYSTEMCTL enable analytics.service analytics.timer
